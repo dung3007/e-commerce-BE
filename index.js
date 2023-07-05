c4c51376cb75e8a46911e0caf72e65f98dee9bce
unknown <hoangdung300798@gmail.com>
2023-07-05 22:24:02 +0700

    commit 1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRouter = require('./routes/user')
+const authRouter = require('./routes/auth')
+const productRouter = require('./routes/product')
+const cartRouter = require('./routes/cart')
+const orderRouter = require('./routes/order')
+const stripeRouter = require('./routes/stripe')
+
+const cors = require("cors")
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('DBConnection Successfully!'))
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
+app.use('/api/orders', orderRouter)
+app.use('/api/checkout', stripeRouter)
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Backend server is running!')
+})
